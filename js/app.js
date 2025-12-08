37edda301081a310480fb0c72206631f1915c0ed
Kawan Bastos <kawanbastos021@gmail.com>
2025-12-08 15:19:23 -0300

    final commit

--- js/app.js
+++ js/app.js
@@ -1,30 +1,28 @@
+let total = 0;
+
 function adicionar() {
-    let produto = document.querySelector("#produto").value;
+    let produto = document.getElementById("produto").value;
     let nome = produto.split("-")[0];
-    let valor = produto.split("-")[1]
-    let campoProdutos = document.getElementById("lista-produtos");
-    let valorTotal = document.getElementById("valor-total");    
-    let quantidaDeProdutos = document.getElementById("quantidade").value;  
+    let valor = produto.split("R$")[1];
+    let campoProdutos = document.getElementById("lista-produtos");  
+    let qtdInput = document.getElementById("quantidade").value;  
+    let campoTotal = document.getElementById("valor-total");
+    
+    if (qtdInput <= 0) {
+      qtdInput = 1; // Define a quantidade mínima como 1
+    }
 
-
-     if (quantidaDeProdutos <= 0) {
-        quantidaDeProdutos = 1;
-     }; 
+    campoProdutos.innerHTML += `<section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${qtdInput}x</span> ${nome} <span class="texto-azul">R$${valor}</span>
+        </section>`;
      
-        campoProdutos.innerHTML += `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidaDeProdutos}x</span> ${nome}  <span class="texto-azul">${valor}</span>
-        </section>
-      </section>`;
+    let subtotal  = valor * qtdInput;
+    total += subtotal;
 
-    
-    let valorAtual = parseFloat(valorTotal.innerHTML);
-    let valorAdicional = parseFloat(valor.replace("R$", ""));
-    let valorFinal = valorAtual + (valorAdicional * quantidaDeProdutos);
-    valorTotal.innerHTML = valorFinal.toFixed(2); // Exibe o valor total com duas casas decimais\
-    return;
-  
+    campoTotal.innerText = total.toFixed(2); // Exibe o valor total com duas casas decimais
 
-    
+    return total;
 }
 
 
@@ -33,6 +31,6 @@ function limpar(){
     campoProdutos.innerHTML = "";
     let valorTotal = document.getElementById("valor-total");
     valorTotal.innerHTML = parseFloat(0).toFixed(2); // Zera o valor total e exibe duas casas decimais
-
-    return;
+    total = 0;
+    return total;
 }
